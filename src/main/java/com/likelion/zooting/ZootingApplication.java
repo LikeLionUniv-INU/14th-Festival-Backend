6e61b9742a34708b09cbc207f0991cc2ccfe3caa
yim0327 <yim020327@gmail.com>
2026-04-11 12:47:28 +0900

    feat: initial spring boot setup and project structure

---
+++ src/main/java/com/likelion/zooting/ZootingApplication.java
@@ -0,0 +1,13 @@
+package com.likelion.zooting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZootingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZootingApplication.class, args);
+	}
+
+}
