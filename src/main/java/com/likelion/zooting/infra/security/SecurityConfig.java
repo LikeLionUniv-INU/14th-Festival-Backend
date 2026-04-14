8b0752c74c904dbc244ded77008e7b99a3069667
yim0327 <yim020327@gmail.com>
2026-04-14 17:07:58 +0900

    feat(security): Spring Security 기본 설정 추가

---
+++ src/main/java/com/likelion/zooting/infra/security/SecurityConfig.java
@@ -0,0 +1,29 @@
+package com.likelion.zooting.infra.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        http
+                .csrf(AbstractHttpConfigurer::disable) // CSRF 보호 기능
+                .formLogin(AbstractHttpConfigurer::disable) // 로그인 ui
+                .httpBasic(AbstractHttpConfigurer::disable) // 기본 인증
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(
+                                "/swagger-ui/**",
+                                "/v3/api-docs/**"
+                        ).permitAll()
+                        .anyRequest().permitAll()
+                );
+
+        return http.build();
+    }
+}
