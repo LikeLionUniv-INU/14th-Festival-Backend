bf30661f21e1bfdfe36c032b67cce65474c57835
backstone025 <112708588+backstone025@users.noreply.github.com>
2026-04-20 03:09:40 +0900

    feat(userpreferredanimaltype): UserAnimalType 엔티티 추가

---
+++ src/main/java/com/likelion/zooting/domain/userpreferredanimaltype/entity/UserAnimalType.java
@@ -0,0 +1,28 @@
+package com.likelion.zooting.domain.userpreferredanimaltype.entity;
+
+import com.likelion.zooting.domain.animaltype.entity.AnimalType;
+import com.likelion.zooting.domain.user.entity.User;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)  // JPA만 접근 가능
+@AllArgsConstructor
+@Table(name = "USER_ANIMAL_TYPE")
+public class UserAnimalType {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "USER_ANIMAL_TYPE")
+    private Long userAnimalTypeId;
+
+    @ManyToOne(fetch = FetchType.LAZY)  // 최적화를 위해 USER를 DB에서 가져오지 않도록 설정
+    @JoinColumn(name = "user_id")   // 외래키 설정
+    private User userId;
+
+    @ManyToOne(fetch = FetchType.LAZY)  // 최적화를 위해 ANIMAL_TYPE을 DB에서 가져오지 않도록 설정
+    @JoinColumn(name = "animal_type_id")    // 외래키 설정
+    private AnimalType animalTypeId;
+
+}
