08ef8fe89609a277797eb0270506699eba7c5218
backstone025 <112708588+backstone025@users.noreply.github.com>
2026-04-20 03:07:15 +0900

    feat(userinterest): UserInterest 엔티티 추가

---
+++ src/main/java/com/likelion/zooting/domain/userinterest/entity/UserInterest.java
@@ -0,0 +1,27 @@
+package com.likelion.zooting.domain.userinterest.entity;
+
+import com.likelion.zooting.domain.interest.entity.Interest;
+import com.likelion.zooting.domain.user.entity.User;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)  // JPA만 접근 가능
+@AllArgsConstructor
+@Table(name = "USER_INTEREST")
+public class UserInterest {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_interest_id")
+    private Long userInterestId;
+
+    @ManyToOne(fetch = FetchType.LAZY)  // 최적화를 위해 USER을 DB에서 가져오지 않도록 설정
+    @JoinColumn(name = "user_id")   // 외래키 설정
+    private User userId;
+
+    @ManyToOne(fetch = FetchType.LAZY)  // 최적화를 위해 INTEREST를 DB에서 가져오지 않도록 설정
+    @JoinColumn(name = "interest_id")   // 외래키 설정
+    private Interest interestId;
+}
