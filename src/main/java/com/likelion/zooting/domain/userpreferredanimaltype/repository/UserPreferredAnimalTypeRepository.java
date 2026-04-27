af6e1eb381fe0d475fe2260089a7232959a56150
backstone025 <112708588+backstone025@users.noreply.github.com>
2026-04-28 02:03:14 +0900

    feat(UserPreferredAnimalType): UserPreferredAnimalType repository 생성

---
+++ src/main/java/com/likelion/zooting/domain/userpreferredanimaltype/repository/UserPreferredAnimalTypeRepository.java
@@ -0,0 +1,7 @@
+package com.likelion.zooting.domain.userpreferredanimaltype.repository;
+
+import com.likelion.zooting.domain.userpreferredanimaltype.entity.UserPreferredAnimalType;
+import org.springframework.data.repository.CrudRepository;
+
+public interface UserPreferredAnimalTypeRepository extends CrudRepository<UserPreferredAnimalType, Long> {
+}
