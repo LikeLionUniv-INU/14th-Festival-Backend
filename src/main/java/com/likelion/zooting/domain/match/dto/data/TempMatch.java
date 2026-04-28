88c5000303c561078ce2634dade51a32d3ebdc01
backstone025 <112708588+backstone025@users.noreply.github.com>
2026-04-28 22:36:33 +0900

    refactor(match): mapper적용 후 필요없어진 reporitory 삭제

--- src/main/java/com/likelion/zooting/domain/match/dto/data/TempMatch.java
+++ src/main/java/com/likelion/zooting/domain/match/dto/data/TempMatch.java
@@ -7,7 +7,7 @@ package com.likelion.zooting.domain.match.dto.data;
  * @param femaleUserIndex
  * @param score
  */
-public record TempMatch(
+public record TempIndexMatch(
         Integer maleUserIndex,      // 남성 사용자의 index 값(점수 매칭 보드 scoreBoard의 row)
         Integer femaleUserIndex,    // 여성 사용자의 index 값(점수 매칭 보드 scoreBoard의 column)
         Integer score               // 두 관계 간 최종 매칭 점수
