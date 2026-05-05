816a026bae77d737636e3eacec4b3e402fd45a0e
yim0327 <yim020327@gmail.com>
2026-05-05 19:16:19 +0900

    fix: Repository 애노테이션 누락 추가

--- src/main/java/com/likelion/zooting/domain/match/repository/MatchRepository.java
+++ src/main/java/com/likelion/zooting/domain/match/repository/MatchRepository.java
@@ -6,9 +6,11 @@ import com.likelion.zooting.domain.user.entity.Status;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface MatchRepository extends JpaRepository<Matches, Long> {
 
     boolean existsByMaleUser_GenderAndMaleUser_Status(Gender gender, Status status);
